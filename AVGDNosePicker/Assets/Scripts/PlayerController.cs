b37d03bc0602e098856a78b0dd1a1f2bbe6e97c3
Ezequiel Arrieta <ezequielarrieta83@gmail.com>
2018-09-17 23:32:24 -0500

    Created Build and fixed infinite clicking

--- AVGDNosePicker/Assets/Scripts/PlayerController.cs
+++ AVGDNosePicker/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 	public GameObject Arm;
 	public Vector3 pos;
 	public Vector3 startPos;
+	int clickCount = 0;
 	// Use this for initialization
 	void Start () 
 	{
@@ -16,13 +17,15 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetButtonDown ("Fire1")) 
+		if (Input.GetButtonDown ("Fire1") && clickCount == 0  ) 
 		{
 			transform.Translate(pos);
+			clickCount++;
 		}
-		if(Input.GetButtonDown("Fire2"))
+		if(Input.GetButtonDown("Fire2") && clickCount == 1)
 		{
 			transform.Translate(startPos);
+			clickCount--;
 		}
 
 	}
