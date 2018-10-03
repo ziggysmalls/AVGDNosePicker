d83d0e3681ae3620b1a7f85ced9a98e6ed4fc4b2
Ezequiel Arrieta <ezequielarrieta83@gmail.com>
2018-10-02 20:25:50 -0500

    Updated Version with new scripts and sprite switching.

---
+++ AVGDNosePicker/Assets/Scripts/HoldButton.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HoldButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    private bool pointerDown;
+    public float pointerDownTimer;
+
+    [SerializeField]
+    public float requiredHoldTime;
+
+    public UnityEvent onLongClick;
+
+    [SerializeField]
+    private Image fillImage;
+
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pointerDown = true;
+        Debug.Log("OnPointerDown");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Reset();
+        Debug.Log("OnPointerUp");
+    }
+
+
+
+    // Use this for initialization
+ //   void Start () {
+		
+//	}
+
+
+	
+	// Update is called once per frame
+	private void Update () {
+        if (pointerDown)
+        {
+            pointerDownTimer += Time.deltaTime;
+            if (pointerDownTimer >= requiredHoldTime)
+            {
+                if (onLongClick != null)
+                    onLongClick.Invoke();
+
+                Reset();
+            }
+            fillImage.fillAmount = pointerDownTimer / requiredHoldTime;
+        }
+
+    }
+
+
+
+    private void Reset()
+    {
+        pointerDown = false;
+        pointerDownTimer = 0;
+        fillImage.fillAmount = pointerDownTimer / requiredHoldTime;
+    }
+}
--- AVGDNosePicker/Assets/Scripts/PlayerController.cs
+++ AVGDNosePicker/Assets/Scripts/PlayerController.cs
@@ -11,13 +11,19 @@ public class PlayerController : MonoBehaviour
     public Sprite BoogArm2;
     public Sprite BoogArm3;
     public Sprite BoogArm4;
+    public Sprite BoogArm5;
+    public Sprite BoogArm6;
+    public Sprite BoogArm7;
+    public Sprite BoogArm8;
+    public AudioEvent boogerAudioEvent;
     int clickCount = 0;
     int nosePickCount = 0;
-    public AudioEvent boogerAudioEvent;
     Vector3 playerSpawnPoint;
+    //   public HoldButton but;
+//    HoldButton values = FindObjectOfType<HoldButton>();
 
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start () 
 	{
 		
         playerSpawnPoint = transform.position;
@@ -35,7 +41,7 @@ public class PlayerController : MonoBehaviour
         if(nosePickCount == 2)
         {
             this.GetComponent<SpriteRenderer>().sprite = BoogArm2;
-            boogerAudioEvent.Play(playerSpawnPoint);
+
            
         
         }
@@ -43,29 +49,67 @@ public class PlayerController : MonoBehaviour
         if(nosePickCount == 3)
         {
             this.GetComponent<SpriteRenderer>().sprite = BoogArm3;
-           boogerAudioEvent.Play(playerSpawnPoint);
+
         
 
         }
-        if(nosePickCount >= 4)
+        if(nosePickCount == 4)
         {
             this.GetComponent<SpriteRenderer>().sprite = BoogArm4;
-         boogerAudioEvent.Play(playerSpawnPoint);
+
         
         }
+        if (nosePickCount == 5)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = BoogArm5;
 
-		if (Input.GetButtonDown ("Fire1") && clickCount == 0  ) 
-		{
-			transform.Translate(pos);
-            nosePickCount++;
-            clickCount++;
-		}
-		if(Input.GetButtonDown("Fire2") && clickCount == 1)
+
+        }
+        if (nosePickCount == 6)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = BoogArm6;
+
+
+        }
+        if (nosePickCount == 7)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = BoogArm7;
+
+
+        }
+        if (nosePickCount >= 8)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = BoogArm8;
+
+
+        }
+
+//        HoldButton butScript = GetComponent<HoldButton>(); // may cause issues
+
+
+        //if(but.GetComponent<HoldButton>().requiredHoldTime)                   butScript.GetComponent<HoldButton>().pointerDownTimer >= butScript.GetComponent<HoldButton>().requiredHoldTime
+
+
+
+
+
+        if (Input.GetButtonDown("Fire2") && clickCount == 1)
 		{
 			transform.Translate(startPos);
             boogerAudioEvent.Play(playerSpawnPoint);
 			clickCount--;
 		}
+
+
+		if (Input.GetButtonDown ("Fire1") && clickCount == 0   ) //&& (GameObject.Find("Button").GetComponent<HoldButton>().pointerDownTimer >= GameObject.Find("Button").GetComponent<HoldButton>().requiredHoldTime)) 
+		{
+
+            transform.Translate(pos);
+            nosePickCount++;
+            clickCount++;
+        }
+
+
 
 	}
 }
