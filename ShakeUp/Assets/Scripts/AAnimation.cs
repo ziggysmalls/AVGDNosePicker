5f8db002e1fbe1717ff519b41b6a72477dae72e0
Ezequiel Arrieta <ezequielarrieta83@gmail.com>
2018-10-18 19:33:09 -0500

    Started the project

---
+++ ShakeUp/Assets/Scripts/AAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AAnimation
+{
+	public AAnimation prev;
+	public AAnimation next;
+	public List<Sprite> sprites;
+
+    public int currentFrameIndex;
+    public float interval;
+    public float timer;
+    
+	
+
+}
---
+++ ShakeUp/Assets/Scripts/SpriteAnimator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimator : MonoBehaviour 
+{
+private AAnimation head;
+ private AAnimation currentAnimation;
+ private SpriteRenderer spriteRenderer;
+ public GameObject player;
+	// Use this for initialization
+	void Start () 
+	{
+		spriteRenderer = player.GetComponent<SpriteRenderer>();
+		head = currentAnimation;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		LinkedList(currentAnimation);
+		
+		if (currentAnimation != null)
+        {
+            currentAnimation.timer += Time.deltaTime;
+            if (currentAnimation.timer > currentAnimation.interval )
+            {
+                currentAnimation.timer = 0;
+                currentAnimation.currentFrameIndex++;
+
+                if (currentAnimation.currentFrameIndex < currentAnimation.sprites.Count - 1)
+                    spriteRenderer.sprite = currentAnimation.sprites[currentAnimation.currentFrameIndex];
+                else
+                {
+                    
+                    currentAnimation.currentFrameIndex = 0;
+                }
+
+           
+            }
+		}
+	}
+	void LinkedList(AAnimation animation)
+	{
+		
+	}
+
+
+
+
+}
+
+
+
+
+
+
+
